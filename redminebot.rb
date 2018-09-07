1dedce0994a242b79368d02247440bf8f68b4a30
Marcus Wu <marcus.wu0814@gmail.com>
2018-09-07 11:54:13 +0800

    Release redminebot 0.2.0

--- redminebot.rb
+++ redminebot.rb
@@ -1,8 +1,8 @@
 class Redminebot < Formula
   desc "A Redmine CLI tool write with Swift. 👨‍💻"
   homepage ""
-  url "https://github.com/marcuswu0814/RedmineBot/archive/0.1.0.tar.gz"
-  sha256 "4dbb2d663f91948ab29becd9aeacab9455d5fd287eb7e557d172d46312a49ebd"
+  url "https://github.com/marcuswu0814/RedmineBot/archive/0.2.0.tar.gz"
+  sha256 "9a474611d4de70dc8aa88e9c71eacbf946733f2922ce8edd42f6f178d838928e"
 
   def install
     system "make", "install", "PREFIX=#{prefix}"
